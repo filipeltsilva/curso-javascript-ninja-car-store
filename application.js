a4181d1016bd0f0922a4dc38b0b3a50fcd137aac
Filipe Lemos <contato@filipelemos.com>
2017-11-22 14:24:12 -0200

    Challenge 30

---
+++ application.js
@@ -0,0 +1,78 @@
+(function($) {
+    'use strict';
+
+    var app = (function() {
+        return {
+            getCompanyInfo: function getCompanyInfo() {
+                var ajax = new XMLHttpRequest();
+
+                ajax.open('GET', '/company.json', true);
+                ajax.send();
+                ajax.addEventListener('readystatechange', this.printCompanyInfo, false);
+            },
+
+            init: function init() {
+                this.getCompanyInfo();
+                this.initEvents();
+            },
+
+            initEvents: function initEvents() {
+                $('[data-js="cars-register"]').on('submit', this.submitForm);
+            },
+
+            insertCar: function insertCar() {
+                var fragment = document.createDocumentFragment();
+
+                var carRow = document.createElement('tr');
+                var carImage = document.createElement('td');
+                var carModel = document.createElement('td');
+                var carYear = document.createElement('td');
+                var carPlate = document.createElement('td');
+                var carColor = document.createElement('td');
+
+                var image = document.createElement('img');
+                image.src = $('[data-js="image"]').get().value;
+                carImage.appendChild(image);
+
+                carModel.textContent = $('[data-js="mark-model"]').get().value;
+                carYear.textContent = $('[data-js="year"]').get().value;
+                carPlate.textContent = $('[data-js="license-plate"]').get().value;
+                carColor.textContent = $('[data-js="color"]').get().value;
+
+                carRow.appendChild(carImage);
+                carRow.appendChild(carModel);
+                carRow.appendChild(carYear);
+                carRow.appendChild(carPlate);
+                carRow.appendChild(carColor);
+
+                return fragment.appendChild(carRow);
+            },
+
+            isRequestReady: function isRequestReady() {
+                return this.readyState === 4 && this.status === 200;
+            },
+
+            printCompanyInfo: function printCompanyInfo() {
+                if (!app.isRequestReady.call(this))
+                    return;
+
+                var companyData = JSON.parse(this.responseText);
+                var companyName = $('[data-js="company-name"]').get();
+                var companyPhone = $('[data-js="company-phone"]').get();
+
+                companyName.textContent = companyData.name;
+                companyPhone.textContent = companyData.phone;
+            },
+
+            submitForm: function submitForm(event) {
+                event.preventDefault();
+
+                var carsTable = $('[data-js="cars-table"]').get();
+                carsTable.appendChild(app.insertCar());
+            }
+        };
+    })();
+
+    app.init();
+
+})(window.DOM);
